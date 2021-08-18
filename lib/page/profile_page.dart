43cc5bf78980d16f48a963b266492cbd4e6dd5c0
iremakdgn <irem.akdgn.1996@gmail.com>
2021-08-18 17:31:58 +0300

    first commit

--- lib/page/profile_page.dart
+++ lib/page/profile_page.dart
@@ -61,82 +61,229 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Row(
-          children: [
-            Column(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon(
+            Icons.person_add_alt_1,
+            color: Colors.black,
+          ),
+          onPressed: () {},
+        ),
+        title: Text(
+          "@zarkmuckerberg",
+          style: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: 16, color: Colors.black),
+        ),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            padding: EdgeInsets.only(right: 60.0),
+            icon: Icon(
+              Icons.check_circle,
+              color: Colors.black,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.settings,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              // do something
+            },
+          )
+        ],
+      ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: <
+                  Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 25.0, top: 25),
-                      child: Icon(Icons.person_add_alt_1),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 90.0, top: 25),
-                      child: Row(
-                        children: [
-                          Text(
-                            "@zarkmuckerberg",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 16),
-                          ),
-                          Icon(Icons.check_circle)
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 75.0, top: 25.0),
-                      child: Icon(Icons.settings),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: CircleAvatar(
-                    radius: 30.0,
-                    backgroundImage: AssetImage('assets/images/profil.png'),
-                    backgroundColor: Colors.transparent,
-                  ),
-                ),
-                RaisedButton(
-                  elevation: 5,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0),
-                      side: BorderSide(color: Colors.black)),
-                  onPressed: () {},
-                  padding: EdgeInsets.only(left: 15.0, right: 15.0),
-                  color: Colors.white,
-                  textColor: Colors.black,
-                  child: Text("+ real name", style: TextStyle(fontSize: 12)),
-                ),
-                RaisedButton(
-                  elevation: 5,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0),
-                      side: BorderSide(color: Colors.black)),
-                  onPressed: () {},
-                  padding: EdgeInsets.only(left: 23.0, right: 23.0),
-                  color: Colors.white,
-                  textColor: Colors.black,
-                  child: Text("+ add bio", style: TextStyle(fontSize: 12)),
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                Row(
-                  children: [
-                    Text("follower | 286"),
-                    SizedBox(
-                      width: 100,
-                    ),
-                    Text("following | 123"),
-                  ],
+                CircleAvatar(
+                  radius: 30.0,
+                  backgroundImage: AssetImage('assets/images/profil.png'),
+                  backgroundColor: Colors.transparent,
                 ),
               ],
             ),
-          ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ButtonTheme(
+                  minWidth: 50.0,
+                  height: 25.0,
+                  child: RaisedButton(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                        side: BorderSide(color: Colors.grey)),
+                    onPressed: () {},
+                    color: Colors.white,
+                    textColor: Colors.black,
+                    child: Text("+ real name",
+                        style: TextStyle(
+                            fontSize: 12, fontWeight: FontWeight.bold)),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ButtonTheme(
+                  minWidth: 90.0,
+                  height: 25.0,
+                  child: RaisedButton(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                        side: BorderSide(color: Colors.grey)),
+                    onPressed: () {},
+                    color: Colors.white,
+                    textColor: Colors.black,
+                    child: Text("+ add bio",
+                        style: TextStyle(
+                            fontSize: 12, fontWeight: FontWeight.bold)),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("follower | 286"),
+                SizedBox(
+                  width: 100,
+                ),
+                Text("following | 123"),
+              ],
+            ),
+            SizedBox(height: 20.0),
+            DefaultTabController(
+                length: 2, // length of tabs
+                initialIndex: 0,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Container(
+                        child: TabBar(
+                          labelColor: Colors.green,
+                          unselectedLabelColor: Colors.black,
+                          tabs: [
+                            Tab(text: 'perspectives'),
+                            Tab(text: 'subjects'),
+                          ],
+                        ),
+                      ),
+                      Container(
+                          height: 250, //height of TabBarView
+                          decoration: BoxDecoration(
+                              border: Border(
+                                  top: BorderSide(
+                                      color: Colors.grey, width: 0.5))),
+                          child: TabBarView(children: <Widget>[
+                            Container(
+                              child: Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text('Hologram-Bedouin',
+                                            style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold)),
+                                        IconButton(
+                                          icon: Icon(
+                                            Icons.menu,
+                                            color: Colors.black,
+                                          ),
+                                          onPressed: () {
+                                            // do something
+                                          },
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      ClipRRect(
+                                        borderRadius:
+                                            BorderRadius.circular(8.0),
+                                        child: Image.asset(
+                                          "assets/images/resim.png",
+                                          height: 100,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Card(
+                                        color: Colors.black,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(5.0),
+                                        ),
+                                        child: Text(
+                                          "@zarkmuckerberg veri intense song but felt like I was on molly in tulum",
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text("+3 more friends"),
+                                      IconButton(
+                                        icon: Icon(
+                                          Icons.reply,
+                                          color: Colors.black,
+                                        ),
+                                        onPressed: () {
+                                          // do something
+                                        },
+                                      ),
+                                      Text("22"),
+                                      IconButton(
+                                        icon: Icon(
+                                          Icons.thumb_up,
+                                          color: Colors.black,
+                                        ),
+                                        onPressed: () {
+                                          // do something
+                                        },
+                                      ),
+                                      Text("18"),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              child: Text('Display Tab 2',
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold)),
+                            ),
+                          ]))
+                    ])),
+          ]),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
